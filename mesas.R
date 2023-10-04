19d5e80c34fcf5916072a8f450947d55d796fa97
Beatriz Milz <milz.bea@gmail.com>
2023-10-04 09:10:39 -0300

    mesas agora vem do arquivo exportado

--- mesas.R
+++ mesas.R
@@ -1,6 +1,19 @@
-url_sheet <- "https://docs.google.com/spreadsheets/d/1xea8IfIc69z06u2vI5EotAYd2m5salQfpvrDM6OR83s/edit#gid=1246342455"
+url_sheet <- "~/Downloads/73397-4d34f530-62ab-11ee-ad03-f16b9b3064c7.xlsx"
 
-tab <- googlesheets4::read_sheet(url_sheet, "convidados_por_pessoa") |> 
+tab_raw <- readxl::read_excel(url_sheet, skip = 1) |> 
+  janitor::clean_names() |> 
+  tidyr::fill(nome_do_convite, ddi,
+              ddd_telefone_para_confirmacao_de_presenca,
+              grupo_do_convite,
+              .direction = "down") |> 
+  dplyr::mutate(mesa = as.numeric(mesa)) |> 
+  dplyr::filter(situacao != "Não comparecerá")
+
+
+tab_raw
+
+
+tab <- tab_raw |> 
   dplyr::group_by(mesa) |> 
   dplyr::mutate(numero_pessoas = dplyr::n())
 
@@ -20,7 +33,6 @@ tab |>
   dplyr::filter(numero_pessoas != 8) |> 
   dplyr::group_split(mesa)
 
-tab |> 
-  dplyr::ungroup() |> 
-  dplyr::distinct(mesa, numero_pessoas) |> 
-  dplyr::count(numero_pessoas)
+tab_raw |> 
+  dplyr::arrange(mesa) |> 
+  dplyr::count(mesa) 
